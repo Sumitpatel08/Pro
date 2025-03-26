a2bbaca6cd4fc2eef3263615969843262cf46bc7
King 🜲 <shidoteshika1@gmail.com>
2025-03-26 15:03:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,8 @@ from pyrogram import Client
 from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
+import pyrogram.utils
+pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, CHANNEL_ID, PORT, OWNER_ID
 
